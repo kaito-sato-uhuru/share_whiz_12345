e32d20df90e1043afcef10a2c8f8c30f96760518
R316\R316-2616 <sato.kaito@uhuru.jp>
2026-05-13 16:55:17 +0900

    コミット

--- share_whiz_back/index.js
+++ share_whiz_back/index.js
@@ -1,7 +1,48 @@
 const express = require("express");
 const app = express();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-// localhost:3000で起動
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+app.use('/uploads', express.static('uploads'))
+
+const cors = require('cors')
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
+
 app.listen(3000, () => {
     console.log("Listening on localhost port 3000");
 });
+
+app.post("/posts", upload.single('image'), async (req, res) => {
+  console.log("リクエストボディ",req.body)
+  try {
+    const imagePath = req.file ? `uploads/${req.file.filename}` : null; 
+    const postData = await prisma.task.create({
+      data: {
+        title: req.body.title,
+        body: req.body.body,
+        image: imagePath,
+      },
+    });
+
+   if (postData.image) {
+      postData.image = `http://localhost:3000/${postData.image}`
+      console.log(postData.image)
+    } else {
+      postData.image = null;
+    }
+
+    res.json(postData)
+  } catch(error) {
+    console.log(error)
+    res.status(500).send("タスクの保存に失敗しました")
+  }
+});
+
